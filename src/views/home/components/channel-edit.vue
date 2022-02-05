a5d61e0d794990c6cb9a6a3d7c8be638a6bdb5e4
GTY <a15302948444@gmail.com>
2022-02-05 18:48:30 +0800

    完成了首页 登录页面，正在开发编辑频道

---
+++ src/views/home/components/channel-edit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="channelEdit-contain">
+    <van-cell-group inset>
+      <van-cell>
+        <div slot="title">我的频道</div>
+        <van-button size="mini" round type="danger" plain>编辑</van-button>
+      </van-cell>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'channelEdit'
+}
+</script>
+
+<style></style>
