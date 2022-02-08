5328fcdfa8b93ebb90aa8b5d45f3b3857fc00a6d
GTY <a15302948444@gmail.com>
2022-02-08 14:35:45 +0800

    完成文章组件初始化

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -3,10 +3,24 @@
     <form action="/">
       <van-search v-model="searchText" show-action placeholder="请输入搜索关键词" @search="onSearch" @cancel="onCancel" background="#3296fa" />
     </form>
+    <!-- 搜索历史记录 -->
+    <search-history />
+    <!-- /搜索历史记录 -->
+
+    <!-- 联想建议 -->
+    <search-suggestion />
+    <!-- /联想建议 -->
+
+    <!-- 历史记录 -->
+    <search-result />
+    <!-- /历史记录 -->
   </div>
 </template>
 
 <script>
+import searchSuggestion from '@/views/search/conpoments/search-suggestion.vue'
+import searchResult from '@/views/search/conpoments/search-result.vue'
+import searchHistory from '@/views/search/conpoments/search-history.vue'
 export default {
   name: 'Search',
   data() {
@@ -23,6 +37,11 @@ export default {
       // this.$toast('取消')
       console.log('quxiao')
     }
+  },
+  components: {
+    searchSuggestion,
+    searchResult,
+    searchHistory
   }
 }
 </script>
